8c3bcecd7e0599e3d82b35f63fefc9ebd1713363
= <=>
2021-08-02 23:04:05 -0300

    Ajustes projeto curso

--- src/test/java/com/wallet/HelloWord.java
+++ src/test/java/com/wallet/HelloWord.java
@@ -1,18 +1,19 @@
 package com.wallet;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import junit.framework.TestCase;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("test")
-class WalletApplicationTests {
-
+public class HelloWord extends TestCase{
+	
 	@Test
-	void contextLoads() {
+	public void testHelloWorld() {
+		assertEquals(1,1);
 	}
 
 }
